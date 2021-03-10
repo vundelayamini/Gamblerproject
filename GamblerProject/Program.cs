555ff6d828e7b53410fe48f4b5bc1a22890a5e61
vundelayamini <vundelayamini@gmail.com>
2021-03-10 13:11:36 +0530

    [yamini]ADD: Win or loss

--- GamblerProject/Program.cs
+++ GamblerProject/Program.cs
@@ -4,10 +4,22 @@ namespace GamblerProject
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            int snake=100;
-            int bet = 1
+            static void Main(string[] args)
+            {
+                int snake = 100;
+                int bet = 1
+               Random random = new Random();
+                int checkVal = Random.Next(0, 2);
+                if (checkVal == 1)
+                {
+                    Console.WriteLine("player won");
+                }
+                else
+                {
+                    Console.WriteLine("player loss");
+
+                }
+
+            }
         }
-    }
 }
