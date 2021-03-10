0bd6b95a8ec808d35e423bde9a54adc8d602842b
vundelayamini <vundelayamini@gmail.com>
2021-03-10 12:01:53 +0530

    [yamini]Add: create a Gambler

---
+++ GamblerProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GamblerProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int snake=100;
+            int bet = 1
+        }
+    }
+}
